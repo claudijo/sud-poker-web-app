c665de2aedf55f7e8edbeae106ae7d768f1426d5
claudijo <claudijo.borovic@gmail.com>
2021-07-12 22:21:03 +0200

    Add debounce

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -1,20 +1,32 @@
 export const localCoordinatesFromMouseEvent = (event, scale = 1) => {
-    const rect = event.target.getBoundingClientRect()
-    const {clientX, clientY} = event
-    const x = (clientX - rect.x) / scale
-    const y = (clientY - rect.y) / scale
-    return {x, y}
-}
+  const rect = event.target.getBoundingClientRect();
+  const { clientX, clientY } = event;
+  const x = (clientX - rect.x) / scale;
+  const y = (clientY - rect.y) / scale;
+  return { x, y };
+};
 
-export const throttleEventHandler = callback => {
-    let tick = false
-    return (event) => {
-        if (!tick) {
-            requestAnimationFrame(() => {
-                callback(event)
-                tick = false
-            })
-        }
-        tick = true
+export const throttle = fn => {
+  let tick = false;
+  return (...args) => {
+    if (!tick) {
+      requestAnimationFrame(() => {
+        fn(...args);
+        tick = false;
+      });
     }
+    tick = true;
+  };
+};
+
+export const debounce = fn => {
+  let timeout;
+  return (...args) => {
+    if (timeout) {
+      cancelAnimationFrame(timeout);
+    }
+    timeout = requestAnimationFrame(() => {
+      fn(...args);
+    });
+  };
 }
