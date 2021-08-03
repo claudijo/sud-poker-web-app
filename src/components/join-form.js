700b6e7c1770aada078a2cd5c3b9150f32eaabfe
claudijo <claudijo.borovic@gmail.com>
2021-08-03 22:00:37 +0200

    Basic styling for join form

--- src/components/join-form.js
+++ src/components/join-form.js
@@ -1,4 +1,9 @@
+import classNames from 'classnames';
 import styles from './join-form.module.css';
+import '../styles/grid.css';
+import '../styles/typography.css';
+import '../styles/interaction.css';
+
 
 export default function JoinForm(
   {
@@ -11,56 +16,64 @@ export default function JoinForm(
     onBuyInChange,
   },
 ) {
+
   return (
     <form
-      className={styles.joinForm}
+      className={classNames(styles.form)}
       name="joinForm"
       autoComplete="off"
       noValidate
       onSubmit={onSubmit}
     >
+
       <h1>Join the fun™</h1>
       <div className="avatar"/>
-      <label>
-        Avatar
-        <select
-          name="avatarStyle"
-          value={avatar}
-          onChange={onAvatarChange}
-        >
-          <option value="MALE">Male</option>
-          <option value="FEMALE">Female</option>
-          <option value="GRIDY">Gridy</option>
-        </select>
-      </label>
-      <label>
-        Nickname
-        <input
-          value={nickname}
-          onChange={onNicknameChange}
-          spellCheck="false"
-          type="text"
-          required="required"
-          title="Nickname"
-          minLength="2"
-          maxLength="20"
-          autoFocus="autofocus"
-        />
-      </label>
-      <label>
-        Buy-in
-        <input
-          value={buyIn}
-          onChange={onBuyInChange}
-          type="number"
-          required="required"
-          title="Buy-in"
-          min="1"
-          max="99999"
-        />
-      </label>
-      <button>Cancel</button>
-      <button>Join</button>
+
+        <section className="grid gridMargin">
+          <label className="colGutter col5of12">
+            Avatar style
+            <select
+              name="avatarStyle"
+              value={avatar}
+              onChange={onAvatarChange}
+            >
+              <option value="MALE">Male</option>
+              <option value="FEMALE">Female</option>
+              <option value="GRIDY">Gridy</option>
+            </select>
+          </label>
+          <label className="colGutter col7of12">
+            Nickname
+            <input
+              value={nickname}
+              onChange={onNicknameChange}
+              spellCheck="false"
+              type="text"
+              required="required"
+              title="Nickname"
+              minLength="2"
+              maxLength="20"
+              autoFocus="autofocus"
+            />
+          </label>
+          <label className="colGutter col7of12">
+            Buy-in
+            <input
+              value={buyIn}
+              onChange={onBuyInChange}
+              type="number"
+              required="required"
+              title="Buy-in"
+              min="1"
+              max="99999"
+            />
+          </label>
+        </section>
+        <section>
+          <button>Cancel</button>
+          <button>Join</button>
+        </section>
+
     </form>
   );
 }
