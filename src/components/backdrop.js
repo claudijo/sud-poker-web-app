2e7211e9cb62e81a9e380cff065cdaf91576e303
claudijo <claudijo.borovic@gmail.com>
2021-08-10 15:06:37 +0300

    Backdrop accepts children

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -1,7 +1,9 @@
 import styles from './backdrop.module.css'
 
-export default function Backdrop() {
+export default function Backdrop({ children}) {
   return (
-    <div className={styles.backdrop}/>
+    <div className={styles.backdrop}>
+      {children}
+    </div>
   )
 }
