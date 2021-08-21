b53fe31b5d16200cdaac4dda0e3c0889752e4271
claudijo <claudijo.borovic@gmail.com>
2021-08-21 23:34:51 +0300

    Update canvas rectangle element

--- src/canvas-shapes/rectangle.js
+++ src/canvas-shapes/rectangle.js
@@ -1,42 +1,73 @@
 import AbstractShape from './abstract-shape';
+import { boxPoint } from '../lib/intersects';
 
 class Shape extends AbstractShape {
-    static get observedAttributes() {
-        return [
-            ...AbstractShape.observedAttributes,
-            'width',
-            'height',
-        ]
-    }
+  static get observedAttributes() {
+    return [
+      ...AbstractShape.observedAttributes,
+      'width',
+      'height',
+    ];
+  }
 
-    get height() {
-        return parseInt(this.getAttribute('height'))
-    }
+  get height() {
+    return parseInt(this.getAttribute('height'));
+  }
 
-    set height(value) {
-        this.setAttribute('height', value)
-    }
+  set height(value) {
+    this.setAttribute('height', value);
+  }
 
-    get width() {
-        return parseInt(this.getAttribute('width'))
-    }
+  get width() {
+    return parseInt(this.getAttribute('width'));
+  }
 
-    set width(value) {
-        this.setAttribute('width', value)
-    }
+  set width(value) {
+    this.setAttribute('width', value);
+  }
 
-    draw(ctx, offset = { x: 0, y: 0}) {
-        ctx.beginPath();
-        ctx.rect(this.x + offset.x, this.y + offset.y, this.width, this.height);
-    }
+  getBoundingBox() {
+    const x = this.x + this.offset.x + this.originX * this.width;
+    const y = this.y + this.offset.y + this.originY * this.height;
+
+    return {
+      left: x,
+      right: x + this.width,
+      top: y,
+      bottom: y + this.height,
+    };
+  }
+
+  intersects(point) {
+    const x = this.x + this.offset.x + this.originX * this.width;
+    const y = this.y + this.offset.y + this.originY * this.height;
+
+    return boxPoint(
+      x,
+      y,
+      this.width,
+      this.height,
+      point.x,
+      point.y,
+    );
+  }
+
+  draw(ctx, offset = { x: 0, y: 0 }) {
+    const x =  this.x + this.offset.x + this.originX * this.width;
+    const y =  this.y + this.offset.y + this.originY * this.height;
+
+    ctx.beginPath();
+    ctx.rect(x, y, this.width, this.height);
+    this.fillAndStroke(ctx);
+  }
 }
 
 customElements.get('canvas-rectangle') || customElements.define('canvas-rectangle', Shape);
 
-export default function CanvasRectangle({children, ...props}) {
-    return (
-        <canvas-rectangle {...props}>
-            {children}
-        </canvas-rectangle>
-    )
+export default function CanvasRectangle({ children, ...props }) {
+  return (
+    <canvas-rectangle {...props}>
+      {children}
+    </canvas-rectangle>
+  );
 }
