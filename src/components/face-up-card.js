8ac005f50ebba2f2b840443840a01b12fc2ea860
claudijo <claudijo.borovic@gmail.com>
2021-09-13 16:56:03 +0300

    Animate dimmed and elevated effect for face up cards

--- src/components/face-up-card.js
+++ src/components/face-up-card.js
@@ -1,40 +1,53 @@
 import CanvasRectangle from '../canvas-shapes/rectangle';
 import CanvasText from '../canvas-shapes/canvas-text';
 import { parseRank, parseSuit, suitColor } from '../util/card';
+import { animated, useSpring } from '@react-spring/web';
 
-export default function FaceUpCard({ x, y, globalAlpha, rank, suit }) {
+const AnimatedRectangle = animated(CanvasRectangle);
+const AnimatedText = animated(CanvasText);
+
+export default function FaceUpCard({ x, y, globalAlpha, elevated, dimmed, rank, suit }) {
   const color = suitColor(suit);
 
+  const animatedDimmedProps = useSpring({
+    from: { globalAlpha: dimmed ? globalAlpha  : globalAlpha * 0.75},
+    to: { globalAlpha: dimmed ? globalAlpha * 0.75 : globalAlpha }
+  })
+
+  const animatedElevatedProps = useSpring({
+    from: { x: x - (elevated ?  0 : 4), y: y - (elevated ?  0 : 8)},
+    to: { x: x - (elevated ?  4 : 0), y: y - (elevated ?  8 : 0)},
+  })
+
   return (
-    <CanvasRectangle
-      x={x}
-      y={y}
+    <AnimatedRectangle
+      {...animatedDimmedProps}
+      {...animatedElevatedProps}
       width={50}
       height={70}
       fillStyle="#faffec"
-      shadowColor="#00000066"
-      shadowOffsetX={4}
-      shadowOffsetY={4}
-      globalAlpha={globalAlpha}
+      shadowColor="#00000055"
+      shadowOffsetX={4 + (elevated ? 4 : 0)}
+      shadowOffsetY={4 + (elevated ? 8 : 0)}
     >
-      <CanvasText
+      <AnimatedText
+        {...animatedDimmedProps}
         y={4}
         x={4}
-        globalAlpha={globalAlpha}
         fillStyle={color}
         font="34px CardCharacters"
       >
         {parseRank(rank)}
-      </CanvasText>
-      <CanvasText
+      </AnimatedText>
+      <AnimatedText
+        {...animatedDimmedProps}
         x={20}
         y={38}
-        globalAlpha={globalAlpha}
         fillStyle={color}
         font="28px CardCharacters"
       >
         {parseSuit(suit)}
-      </CanvasText>
-    </CanvasRectangle>
-  )
+      </AnimatedText>
+    </AnimatedRectangle>
+  );
 }
