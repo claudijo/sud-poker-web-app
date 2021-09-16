4ffdb6bd9447d9e27225299cde60bf9452191255
claudijo <claudijo.borovic@gmail.com>
2021-09-16 16:01:29 +0300

    Add font style declaration in separate file

--- src/util/card.js
+++ src/util/card.js
@@ -1,3 +1,5 @@
+import { BLACK_SUITS_COLOR, FACE_UP_CARD_BACKGROUND_COLOR, RED_SUITS_COLOR } from './colors';
+
 export const parseRank = rank => rank === '10' ? '=' : rank.charAt(0).toUpperCase();
 
 export const parseSuit = suit => {
@@ -19,10 +21,10 @@ export const suitColor = suit => {
   switch (suit) {
     case 'hearts':
     case 'diamonds':
-      return '#e16057';
+      return RED_SUITS_COLOR;
     case 'spades':
     case 'clubs':
-      return '#33333';
+      return BLACK_SUITS_COLOR;
     default:
       throw new Error(`Invalid rank "${suit}"`);
   }
@@ -38,11 +40,10 @@ export const rankingDescriptions = [
   'Full house',
   'Four of a kind',
   'Straight flush',
-  'Royal flush'
+  'Royal flush',
 ];
 
 const relevantCardsCount = ranking => {
-  console.log('Winner', rankingDescriptions[ranking], ranking)
   switch (ranking) {
     case 0:
       return 1;
@@ -71,4 +72,15 @@ export const isWinningCard = (winners, card) => {
   }
 
   return false;
+};
+
+export const generateFaceUpCard = (card) => {
+  const svg = `
+<svg version="1.1" width="50" height="70" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="${FACE_UP_CARD_BACKGROUND_COLOR}"/>
+  <text font-family="CardCharacters" font-size="34px" class="rank" fill="${suitColor(card.suit)}" x="4" y="36">${parseRank(card.rank)}</text>
+  <text font-family="CardCharacters" font-size="28px" class="suite" fill="${suitColor(card.suit)}" x="21" y="63">${parseSuit(card.suit)}</text>
+</svg>
+  `;
+  return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
 };
