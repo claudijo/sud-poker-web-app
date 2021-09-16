b2e3977a83247b60bf193b872acd09dff97da987
claudijo <claudijo.borovic@gmail.com>
2021-09-16 16:01:44 +0300

    Add colors

--- src/util/colors.js
+++ src/util/colors.js
@@ -1,20 +1,22 @@
-export const STAGE_BACKGROUND_COLOR = '#ffecb3'
+export const STAGE_BACKGROUND_COLOR = '#ffecb3';
 
 export const TABLE_TEXT_COLOR = '#80cbc4';
-export const TABLE_BACKGROUND_COLOR = '#4db6ac'
-export const TABLE_HIGHLIGHT_COLOR = '#80cbc4'
+export const TABLE_BACKGROUND_COLOR = '#4db6ac';
+export const TABLE_HIGHLIGHT_COLOR = '#80cbc4';
 export const TABLE_SHADOW_COLOR = '#00695c';
 
-export const FACE_UP_CARD_BACKGROUND_COLOR = '#fffde7'
+export const FACE_UP_CARD_BACKGROUND_COLOR = '#fffde7';
+export const BLACK_SUITS_COLOR = '#37474f';
+export const RED_SUITS_COLOR = '#ef5350';
 
 export const NAME_LABEL_BACKGROUND_COLOR = '#43a047';
-export const STACK_LABEL_BACKGROUND_COLOR = "#ef5350"
+export const STACK_LABEL_BACKGROUND_COLOR = '#ef5350';
 
-export const CHIP_STACK_LABEL_BACKGROUND_COLOR = '#ffffff'
-export const CHIP_STACK_LABEL_TEXT_COLOR = '#37474f'
-export const CHIP_COLOR = '#ef5350'
+export const CHIP_STACK_LABEL_BACKGROUND_COLOR = '#ffffff';
+export const CHIP_STACK_LABEL_TEXT_COLOR = '#37474f';
+export const CHIP_COLOR = '#ef5350';
 
-export const BUTTON_BACKGROUND_COLOR = '#b2dfdb'
-export const BUTTON_HOVER_BACKGROUND_COLOR = '#80cbc4'
-export const BUTTON_ACTIVE_BACKGROUND_COLOR = '#4db6ac'
-export const BUTTON_BORDER_COLOR = '#009688'
+export const BUTTON_BACKGROUND_COLOR = '#b2dfdb';
+export const BUTTON_HOVER_BACKGROUND_COLOR = '#80cbc4';
+export const BUTTON_ACTIVE_BACKGROUND_COLOR = '#4db6ac';
+export const BUTTON_BORDER_COLOR = '#009688';
