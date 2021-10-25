bf7976a4a8e83297e683a970a2758d6c1bc6d0c7
claudijo <claudijo.borovic@gmail.com>
2021-10-25 17:26:17 +0300

    Add env vars and update manifest

--- src/socket/client-socket-emitter.js
+++ src/socket/client-socket-emitter.js
@@ -1,3 +1,3 @@
 import ClientSocketEmitter from '../lib/client-socket-emitter';
 
-export const clientSocketEmitter = new ClientSocketEmitter(`ws://${process.env.REACT_APP_HOST}:3000/ws/poker`);
+export const clientSocketEmitter = new ClientSocketEmitter(`ws://${process.env.REACT_APP_POKER_SERVICE_HOST}:${process.env.REACT_APP_POKER_SERVICE_PORT}/ws/poker`);
